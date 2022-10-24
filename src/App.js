972c95757010bc1bbf1e6693b9a5f742d4062461
HysenB <hysi2000@live.com>
2022-10-24 11:23:05 +0200

    PrivateRoute component, Spinner component and useAuthStatus hook

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 // components
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 
 // Pages
 import Explore from './pages/Explore';
@@ -22,7 +23,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Explore />} />
         <Route path='/offers' element={<Offers />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile' element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
